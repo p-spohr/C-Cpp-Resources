851c258e4b80154ce1479fbdd93c2f3b57462c18
Patrick <spohrpatrick@gmail.com>
2025-11-28 12:15:23 +0100

    practice counting digits using case and switch

---
+++ count_digits/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+    int c, i, nwhite, nother, ndigit[10];
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; i++) {
+        ndigit[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        switch (c) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                ndigit[c - '0']++;
+                break;
+            case ' ':
+            case '\n':
+            case '\t':
+                nwhite++;
+                break;
+            default:
+                nother++;
+                break;
+        }
+    }
+    
+    printf("digits\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d: %d\n", i, ndigit[i]);
+    }
+
+    printf("\nwhite space = %d | other = %d", nwhite, nother);
+
+    return 0;
+
+}
